f7734bc060142b346a5fc241136b40d9d5239c8d
cleverlmf <clf.nameless@gmail.com>
2022-06-29 08:11:01 -0300

    conteudo passado 28/06

---
+++ javascript/alomundo.js
@@ -0,0 +1,5 @@
+function concluirAula(){   
+    document.getElementById('text').
+    innerHTML ="Aula 1- Alô Mundo Java - Aula Concluída em:"+Date()
+   }
+   
---
+++ readme.md
@@ -0,0 +1,7 @@
+<h1>Curso de Javascript</h1>
+
+>Apostila online
+
+```
+https://developer.mozilla.org/pt-BR/docs/Web/JavaScript
+```
